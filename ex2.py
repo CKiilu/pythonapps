b42c86d287e5d434171f1f84e9fdf48aed358374
CKiilu <cndeti@gmail.com>
2016-01-31 16:32:25 +0300

    ex2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+#A comment, it's invisible
+#Everything after # is ignored as a comment
+
+print "I could have code like this"
+
+#You can use a comment to disable functions
+
+#print "This won't run"
+
+print "this will run"

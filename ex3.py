a23fa442f2d18e48384cae39a4fb1fc6d569216b
CKiilu <cndeti@gmail.com>
2016-01-31 16:53:58 +0300

    ex3 float drill

--- ex3.py
+++ ex3.py
@@ -2,20 +2,20 @@
 print ("I will now count my chickens:")
 
 #Print strings and results of calculations 
-print "Hens", 25+30/6
-print "Roosters", 100-25*3%4
+print "Hens", 25.0+30.0/6.0
+print "Roosters", 100.0-25.0*3.0%4.0
 
 #Prints a string
 print "Now I will count the eggs:"
 
 #prints result of a calculation
-print 3+2+1-5+4%2-1/4+6
+print 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0
 
 #Prints a string
 print "Is it true that 3 + 2 < 5 - 7?"
 
 #Return true or false
-print 3+2<5-7
+print 3.0+2.0<5.0-7.0
 
 #Print strings and results of calculations
 print "What is 3+2?", 3+2
